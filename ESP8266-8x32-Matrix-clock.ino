90856a816d49ae86c6e4d540dc672fa45c2b2e1c
John Rogers <k1wiz@wizworks.net>
2023-07-06 16:38:46 -0400

    Update ESP8266-8x32-Matrix-clock.ino

--- ESP8266-8x32-Matrix-clock.ino
+++ ESP8266-8x32-Matrix-clock.ino
@@ -18,13 +18,13 @@ int dx = 0;
 int dy = 0;
 byte del = 0;
 int h, m, s;
-int utcOffset = 7;  // UTC +7
+int utcOffset = -4;  // UTC +/- value
 long epoch;
 long localMillisAtUpdate;
 int day, month, year, dayOfWeek;
 int summerTime = 0;
 
-const int utcOffsetInSeconds = utcOffset * 3600;
+const float utcOffsetInSeconds = utcOffset * 3600;  // changed to float since some timezones are half hours
 
 // Define NTP Client to get time
 WiFiUDP ntpUDP;
@@ -45,8 +45,8 @@ NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
 // =======================================================================
 // CHANGE YOUR CONFIG HERE:
 // =======================================================================
-const char* ssid = "XuanLiem";          // SSID of local network
-const char* password = "haydoiday@75";  // Password on network
+const char* ssid =     "";          // SSID of local network
+const char* password = "";  // Password on network
 
 void setup() {
   Serial.begin(115200);
@@ -212,70 +212,10 @@ int showChar(char ch, const uint8_t* data) {
   return w;
 }
 
-// =======================================================================
-int dualChar = 0;
-
-unsigned char convertPolish(unsigned char _c) {
-  unsigned char c = _c;
-  if (c == 196 || c == 197 || c == 195) {
-    dualChar = c;
-    return 0;
-  }
-  if (dualChar) {
-    switch (_c) {
-      case 133: c = 1 + '~'; break;                               // 'ą'
-      case 135: c = 2 + '~'; break;                               // 'ć'
-      case 153: c = 3 + '~'; break;                               // 'ę'
-      case 130: c = 4 + '~'; break;                               // 'ł'
-      case 132: c = dualChar == 197 ? 5 + '~' : 10 + '~'; break;  // 'ń' and 'Ą'
-      case 179: c = 6 + '~'; break;                               // 'ó'
-      case 155: c = 7 + '~'; break;                               // 'ś'
-      case 186: c = 8 + '~'; break;                               // 'ź'
-      case 188:
-        c = 9 + '~';
-        break;  // 'ż'
-      //case 132: c = 10+'~'; break; // 'Ą'
-      case 134: c = 11 + '~'; break;  // 'Ć'
-      case 152: c = 12 + '~'; break;  // 'Ę'
-      case 129: c = 13 + '~'; break;  // 'Ł'
-      case 131: c = 14 + '~'; break;  // 'Ń'
-      case 147: c = 15 + '~'; break;  // 'Ó'
-      case 154: c = 16 + '~'; break;  // 'Ś'
-      case 185: c = 17 + '~'; break;  // 'Ź'
-      case 187: c = 18 + '~'; break;  // 'Ż'
-      default: break;
-    }
-    dualChar = 0;
-    return c;
-  }
-  switch (_c) {
-    case 185: c = 1 + '~'; break;
-    case 230: c = 2 + '~'; break;
-    case 234: c = 3 + '~'; break;
-    case 179: c = 4 + '~'; break;
-    case 241: c = 5 + '~'; break;
-    case 243: c = 6 + '~'; break;
-    case 156: c = 7 + '~'; break;
-    case 159: c = 8 + '~'; break;
-    case 191: c = 9 + '~'; break;
-    case 165: c = 10 + '~'; break;
-    case 198: c = 11 + '~'; break;
-    case 202: c = 12 + '~'; break;
-    case 163: c = 13 + '~'; break;
-    case 209: c = 14 + '~'; break;
-    case 211: c = 15 + '~'; break;
-    case 140: c = 16 + '~'; break;
-    case 143: c = 17 + '~'; break;
-    case 175: c = 18 + '~'; break;
-    default: break;
-  }
-  return c;
-}
 
 // =======================================================================
 
 void printCharWithShift(unsigned char c, int shiftDelay) {
-  c = convertPolish(c);
   if (c < ' ' || c > '~' + 25) return;
   c -= 32;
   int w = showChar(c, font);
